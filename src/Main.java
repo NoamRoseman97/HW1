1842c0e53fddba87af8b4300448d3d86cff65e5b
NoamRoseman97 <Noam.roseman@gmail.com>
2023-01-15 22:43:36 +0200

    test project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world how u doin!");
+        int num = 0;
+        if (num > 0)
+        {
+            System.out.println("Positive number");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        }
+        else {
+            System.out.println("Negative number");
+
+        }
+
+
     }
+
 }
